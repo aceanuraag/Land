02f0293f0e2b430129592c473a72daa4103e3790
Anurag <150906478+aceanuraag@users.noreply.github.com>
2023-11-17 13:02:35 +0530

    ""

--- landing/src/App.js
+++ landing/src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import {BrowserRouter} from "react-router-dom";
+import Header from './components/Header';
+import SideBar from './components/SideBar';
+import Body from './components/Body';
 
 function App() {
   return (
     <BrowserRouter >
     <div className="App">
-    <h1>This  is App Anurag</h1>
+      <Header/>
+      <div className= "flex">
+        <SideBar/>
+        <Body/>
+      </div>
     </div>
     </BrowserRouter>
   );
