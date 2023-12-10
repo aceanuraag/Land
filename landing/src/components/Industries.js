0da733db797e57cc887f7a8d1f8b3e5572e5c226
Anurag <150906478+aceanuraag@users.noreply.github.com>
2023-12-10 18:50:56 +0545

    ""

--- landing/src/components/Industries.js
+++ landing/src/components/Industries.js
@@ -1,44 +1,58 @@
 import React from "react";
-// import Link from "react-bootstrap/Link"
+import './Industries.css';
 
 const Industries = () => {
   return (
-    <div class="w3-container w3-teal">
-    
-      <h1 title="investors" style={{marginLeft:"100vh"}}> Investors </h1>
-      <p className="sub-title" style={{marginLeft:"70vh"}}>
+    <div className="w3-container w3-teal">
+      <h1 title="investors" style={{ marginLeft: "100vh" }}> Investors </h1>
+      <p className="sub-title" style={{ marginLeft: "70vh" }}>
         Maximizing shareholder value with good corporate governance
       </p>
-     
+
       <div>
-        <ul style={{ display: "flex", justifyContent: "space-around" }}>
-          <li class="w3-container" style={{ listStyle: "none" }}>
-          <img src="https://www.infosys.com/content/dam/infosys-web/en/global-resource/18/banner/annual-report-2023-mob.jpg" class="w3-round" alt="Norway"/>
+        <ul style={{ display: "flex", justifyContent: "space-around", padding: "10px 0" }}>
+          <li className="w3-container" style={{ listStyle: "none" }}>
+          <img
+  src="https://www.infosys.com/content/dam/infosys-web/en/global-resource/18/banner/annual-report-2023-mob.jpg"
+  alt="Image"
+  className="zoomable-image"
+  style={{ maxWidth: "100%", height: "auto" }}
+/>
+
           </li>
-          <li class="w3-container" style={{ listStyle: "none" }}>
-            <h1>Investor central</h1>
+          <li className="w3-container" style={{ listStyle: "none"}}>
+            <h1 style={{margin:"13px "}}>Investor central</h1>
             <ul style={{ listStyle: "none" }}>
-              <li>
-                <h5>Results</h5>
-                <h4>Second Quarter FY24</h4>
-              </li>
-              <h6>_______________________________________________________</h6>
-              <li>
-                <h5>Archived webcast</h5>
-                <h4>Q2 FY24 Press Conference</h4>
-              </li>
-              <h6>_______________________________________________________</h6>
+              <InvestorColumn title='Result' items={['Second Quarter FY24']} />
+              <hr className="divider" />
+              <InvestorColumn title='Archived webcast' items={['Q2 FY24 Press Conference']} />
+              <hr className="divider" />
               <li>
                 <h5>Event</h5>
                 <h4>The 42<sup>th</sup> Annual General Meeting</h4>
-              </li>  
-              <h6>_______________________________________________________</h6> 
-              <button>VIEW ALL</button>       
+              </li>
+              <hr className="divider" />
+              <button>VIEW ALL</button>
             </ul>
           </li>
         </ul>
       </div>
     </div>
+  );
+};
+
+const InvestorColumn = ({ title, items }) => {
+  return (
+    <div>
+      <h5>{title}</h5>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index} style={{ listStyle: "none", display: "flex" }}>
+            <h4>{item}</h4>
+          </li>
+        ))}
+      </ul>
+    </div> 
   );
 };
 
